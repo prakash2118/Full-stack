7105f9ba3a2744decb57afef60dcb3414209cc9b
prakash2118 <90789267+prakash2118@users.noreply.github.com>
2023-04-04 19:56:21 +0530

    Create script.js

---
+++ Task 2/Calculator/script.js
@@ -0,0 +1,12 @@
+let result = document.getElementById('result');
+
+function input(num) {  result.value += num;
+}
+
+function clearResult() {
+result.value = '';
+}
+
+function calculate() {
+result.value = eval(result.value);
+}
